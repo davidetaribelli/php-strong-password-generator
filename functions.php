f67f8425043312c06a93d9f529e1c600473455f5
davidetaribelli <davide.taribelli@gmail.com>
2023-06-12 10:26:55 +0200

    fix bug function

--- functions.php
+++ functions.php
@@ -1,16 +1,17 @@
 <?php
 function generatePassword($length)
 {
-    $chars = 'ABCDEFFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+0123456789';
-    // $upperCase = 'ABCDEFFGHIJKLMNOPQRSTUVWXYZ';
-    // $smallCase = 'abcdefghijklmnopqrstuvwxyz';
-    // $numbers = '0123456789';
-    // $symbols = '!@#$%^&*()-_=+';
+    // $chars = 'ABCDEFFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+0123456789';
+    $upperCase = 'ABCDEFFGHIJKLMNOPQRSTUVWXYZ';
+    $lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    $numbers = '0123456789';
+    $symbols = '!@#$%^&*()-_=+';
+    $chars = $upperCase . $lowerCase . $numbers . $symbols;
     $password = '';
 
     for ($i = 0; $i < $length; $i++) {
         // Seleziono un carattere casuale dalla lista dei caratteri disponibili
-        $randomChars = rand(0, strlen($chars - 1 ));
+        $randomChars = rand(0, strlen($chars));
         // Passo i caratteri selezionati a password
         $password .= $chars[$randomChars];
     }
